197d54360c5cfc5b72c24a672dc0be2029efef16
falsegenius1 <falsegenius1@gmail.com>
2024-08-20 20:40:49 +0530

    feat: add difficulty level

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.post('/generate-quiz', async (req, res) => {
-    const { topic } = req.body
+    const { topic, difficulty } = req.body
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
         const prompt = `You are a quiz generator expert.
 The topic is as follows: ${topic}
-Perform the following task for the topic provided:
+Difficulty is as follows: ${difficulty}
+Perform the following task for the topic provided as per the difficulty level:
 Generate 10 quiz related to the topic and output the quiz in following text format:
 {"title":"title of the quiz","quizzes": [{"question": "Question 1", "options": ["Option A", "Option B", "Option C", "Option D"], "correct_option": "Option A"}, {"question": "Question 2", "options": ["Option A", "Option B", "Option C", "Option D"], "correct_option": "Option B"}, ..., {"question": "Question 10", "options": ["Option A", "Option B", "Option C", "Option D"], "correct_option": "Option C"}]}.
 Make sure the question and options are precise.
